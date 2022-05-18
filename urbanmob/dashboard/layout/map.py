f92070a33c8369772bee18f0d074d495e3f8093b
Mher Khachatryan <mkhachatryan@krisp.ai>
2022-05-18 21:17:54 +0400

    our data added to map

--- urbanmob/dashboard/layout/map.py
+++ urbanmob/dashboard/layout/map.py
@@ -1,10 +1,17 @@
 import plotly.express as px
 from dash import dcc
 
-px.set_mapbox_access_token(open("/Users/mkhachatryan/Codes/ASDS-Data-Visualisation/data/mapbox_token.txt").read())
-df = px.data.carshare()
-fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon", color="peak_hour",
-                        size="car_hours",
-                        color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
+from data_processing import data
+
+
+px.set_mapbox_access_token(
+        open(
+                "/Users/mkhachatryan/Codes/ASDS-Data-Visualisation/data/mapbox_token.txt").read(
+
+                ))
+
+fig = px.scatter_mapbox(data, lat="pickup_latitude", lon="pickup_longitude", color="passenger_count",
+                        size="trip_duration",
+                        color_continuous_scale=px.colors.cyclical.IceFire)
 
 map_layout = dcc.Graph(id="example_map", figure=fig)
